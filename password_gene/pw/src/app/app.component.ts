abf4666e7e19d8dcac13907b0e17334f420a1dee
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-03 18:27:11 +0100

    project_1: password generator

---
+++ password_gene/pw/src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  password = '';
+  passwordLength = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+  onChangeLength(value: string) {
+    const parseValue = parseInt(value);
+
+    if (!isNaN(parseValue)) {
+      this.passwordLength = parseValue;
+    }
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * generatedPassword.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+}
