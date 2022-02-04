11dfeb4b7eb45bdd5d52410073921c779580f161
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-04 12:46:46 +0100

    Project_4: create ngClass clone

--- pages/src/app/class.directive.ts
+++ pages/src/app/class.directive.ts
@@ -7,7 +7,13 @@ export class ClassDirective {
   constructor(private element: ElementRef) {
   }
 
-  @Input() set appClass(color: string) {
-    this.element.nativeElement.style.backgroundColor = color;
+  @Input('appClass') set backgroundColor(classObj: any) {
+    for (let key in classObj) {
+      if (classObj[key]) {
+        this.element.nativeElement.classList.add(key);
+      } else {
+        this.element.nativeElement.classList.remove(key);
+      }
+    }
   }
 }
