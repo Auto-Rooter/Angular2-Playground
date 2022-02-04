4ee5454ecf115e159723d64988d821aa8777e17b
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-04 10:44:52 +0100

    Project_4: build pagination

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Miskolc City',
+      url: 'https://images.unsplash.com/photo-1579988936083-16d8215bb12e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWlza29sY3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Hiking',
+      url: 'https://images.unsplash.com/uploads/141148589884100082977/a816dbd7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGhpa2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'Snow',
+      url: 'https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c25vd3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    }
+  ];
 }
