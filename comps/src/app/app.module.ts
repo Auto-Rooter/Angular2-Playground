a0762609a68bf8e8320f24322138296ad6bc9cb0
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-08 18:25:55 +0100

    Project_5: collection home comp

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
+import { CollectionsHomeComponent } from './collections/collections-home/collections-home.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionsHomeComponent
   ],
   imports: [
     BrowserModule,
