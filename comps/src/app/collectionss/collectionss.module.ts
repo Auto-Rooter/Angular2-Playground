5f6441cb2b818ed597d111158d18f93e24c3f669
Hadi Assalem <hassalem@diamonddiagnostics.com>
2022-02-08 17:08:36 +0100

    Project_5: generate Modules

---
+++ comps/src/app/collectionss/collectionss.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionssRoutingModule } from './collectionss-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CollectionssRoutingModule
+  ]
+})
+export class CollectionssModule { }
